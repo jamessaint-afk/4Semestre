16811e83cbd9afb3f087f5c64cd7b9993e630f72
JAMES RIBEIRO DA SILVA <51155097840@info.local>
2026-08-11 16:39:18 -0300

    .

---
+++ React Nitive/ReactListNative/src/components/tasklist/TaskListStyle.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const TaskListStyle = StyleSheet.create({
+    taskListContainer : {
+        flex : 1,
+        borderWidth: 3,
+        borderStyle: "solid",
+        borderColor: "green"
+    }
+})
